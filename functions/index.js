43ffc0c7c205d93e1b11780cdbef76b44df5cfd3
Luis Eduardo Esquivias <EsquizoDEV>
2022-07-02 16:41:14 -0500

    initial config

---
+++ functions/index.js
@@ -0,0 +1,60 @@
+import express from 'express';
+const app = express()
+
+var port = normalizePort(9999 || process.env.PORT)
+app.set('port', port)
+
+import http from 'http'
+
+const server = http.createServer(app)
+
+server.listen(port)
+server.on('error', onError);
+server.on('listening', onListening)
+
+
+
+function normalizePort(num){
+    const port = parseInt(num, 10)
+
+    if(isNaN(port)){
+        return val
+    }
+
+    if(port > 0){
+        return port
+    }
+
+    return false
+}
+
+function onError(err){
+    if(error.syscall !== 'listen'){
+        throw err
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe' + port
+        : 'Port' + port;
+
+        switch(err.code){
+            case 'EACCESS':
+                console.error(bind + ' requieres elevated privileges')
+                process.exit(1)
+            case 'EADDRINUSE':
+                console.error(bind + ' is already in use');
+                process.exit(1)
+            default:
+                throw err;
+        }
+}
+
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+
+    console.log('Listening on ' + bind)
+}
+
