80161de1460278a505e4276d86e9e38a11c51f40
Christopher Ortega <chrortegita@gmail.com>
2022-07-04 22:25:23 -0500

    production line model

--- functions/models/production.js
+++ functions/models/production.js
@@ -1,2 +1,24 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
+const { ObjectId } = mongoose.Types
+
+const production = new Schema({
+    _id:             ObjectId,
+    mixing:         Boolean,
+    start:          Date,
+    end:            Date,
+    updated:        Date,
+    orders:         [ObjectId],
+    tasks:          [ObjectId],
+    activeTasks:    [ObjectId],
+    products: [
+        {
+            id:         ObjectId,
+            pct:        Number,
+            name:       String,
+            prodCost:   Number,
+        }
+    ],
+})
+
+export default production
