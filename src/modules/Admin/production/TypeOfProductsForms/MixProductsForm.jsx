6683b8389730654780374bc4a90df92ebecfc832
LuisH86 <108955151+LuisH86@users.noreply.github.com>
2022-07-29 14:55:15 -0600

    Mix Stepper
    Mix creation in a stepper, error handling missing

--- src/modules/Admin/production/TypeOfProductsForms/MixProductsForm.jsx
+++ src/modules/Admin/production/TypeOfProductsForms/MixProductsForm.jsx
@@ -4,7 +4,8 @@ import React, { useState, useEffect, useRef } from 'react'
 import { 
     Autocomplete, Box, Button, 
     TextField, Typography, useTheme, 
-    Fab 
+    Fab,
+    Stepper, Step, StepLabel, StepContent, Paper, 
 } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 import CameraIcon from '@mui/icons-material/AddPhotoAlternate';
@@ -22,6 +23,8 @@ const amounts = ["25","30","40","50","60","70","80","90"]
 
 export const MixProductsForm = () => {
     const theme = useTheme(BV_THEME);
+    //* STEPPER
+    const [activeStep, setActiveStep] = useState(0)
     
     //*DATA STATES
     const [strains, setStrains] = useState([])
@@ -220,7 +223,8 @@ export const MixProductsForm = () => {
             })
             return
         }
-        setShowFinal(true)
+        handleNext()
+        
         //*OK THE MIX HAVE THE RIGHT LENGTH, THE TOTAL OF AMOUNT IS 100% ? 
         
     }
@@ -349,6 +353,137 @@ export const MixProductsForm = () => {
         }
     }, [mix])
 
+    //*********** STEPPER
+    const steps = [
+        {
+          label: 'Set strains in Mix',
+          description: `Please enter the strains in your Mix`,
+        },
+        {
+          label: 'Mix Name and Price',
+          description:
+            'Please provide the name of your mix and the price to use',
+        },
+    ];
+      
+    const handleNext = () => {
+        setShowFinal(true)
+        /*const {errors, errorMapped} = mapErrors()
+        if(errors.length > 0 ){
+            let failed
+            switch(activeStep){
+                case 0:
+                    if(errorMapped["name"]){
+                        setError({
+                            ...error,
+                            ...errorMapped
+                        })
+                        return
+                    }
+                    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+                    break;
+                case 1:
+                    failed = errorMapped["seeding"] || errorMapped["harvest"] || errorMapped["day"] || errorMapped["night"] || errorMapped["price"]
+                    if(failed){
+                        setError({
+                            ...error,
+                            ...errorMapped
+                        })
+                        return
+                    }
+                    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+                    break;
+                case 2:
+                    failed = errorMapped["seedId"] || errorMapped["provider"] || errorMapped["status"]
+                    if(failed){
+                        setError({
+                            ...error,
+                            ...errorMapped
+                        })
+                        return
+                    }
+                default:
+                    break;
+            }
+
+            return
+        }*/
+        setActiveStep((prevActiveStep) => prevActiveStep + 1)
+        
+    };
+    
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+    
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+    const isLastStep = (index) => {
+        return index == steps.length - 1
+
+    };
+
+    
+
+    const getStepContent = (step,index) => {
+        return ( 
+            <>
+            <Typography sx={{display:{xs:"none", sm:"flex"}}}>{step.description}</Typography>
+            <Box sx={{ mb: 2 }}>
+                <div>
+                    <Button
+                        variant="contained"
+                        onClick={isLastStep(index) ? handleSendMixData : handleNext}
+                        sx={()=>({...BV_THEME.button.standard,mt: 1, mr: 1,})}
+                        
+                    >
+                        {isLastStep(index) ? 'Save Mix' : 'Continue'}
+                    </Button>
+                    
+                    <Button
+                        disabled={index === 0}
+                        onClick={handleBack}
+                        sx={{ mt: 1, mr: 1 }}
+                    >
+                        Back
+                    </Button>
+                </div>
+            </Box>
+            </>
+        )
+    }
+
+    const getMobileStepperButtons = (index) => {
+        return (
+            <Box sx={{width:"100%", mb: 2 ,display:{xs:"flex",sm:"none"}, justifyContent: 'space-evenly' }}>
+                
+                <Button
+                        disabled={index === 0}
+                        onClick={handleBack}
+                        sx={()=>({...BV_THEME.button.standard, color:"white_btn"})}
+                        variant="outlined"
+                    >
+                        Back
+                    </Button>
+
+
+                    <Button
+                        variant="contained"
+                        onClick={isLastStep(index) ? handleSendMixData : handleNext}
+                        sx={()=>({...BV_THEME.button.standard})}
+                        
+                    >
+                        {isLastStep(index) ? 'Save Mix' : 'Continue'}
+                    </Button>
+                    
+                    
+                
+            </Box>
+        )
+
+    }
+
     
   return (
     <div style={{paddingLeft:"10vw", paddingRight:"10vw"}}>
@@ -361,135 +496,192 @@ export const MixProductsForm = () => {
         actions={dialog.actions}
         />
         
-        <Box sx={{display:"flex", width:"100%", justifyContent:"space-between",justifyItems:"center", alignItems:"center", flexDirection:"column"}}>
-            <Box sx={
+        <Box sx={{display:"flex", width:"100%", marginTop:"5vh",justifyContent:"center",justifyItems:"center", alignItems:"center", flexDirection:"column"}}>
+        
+        
+        
+        
+            <Box sx={{ width: "90%", display:"flex", flexDirection:{xs:"column",sm:"row"} }}>
+                            
+                <Box justifyContent={"space-evenly"} sx={{ width: "90%", display:{xs:"flex", sm:"none"}}}>
+                    <Stepper activeStep={activeStep} >
+                        {steps.map((step, index) => (
+                            <Step key={step.label}>
+                                <StepLabel sx={{fontSizeAdjust:"20px"}}>
+                                    {step.label}
+                                </StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                </Box>
+
+                <Box sx={{ width: "35%", display:{xs:"none", sm:"inline-block"}}}>
+                    <Stepper activeStep={activeStep} orientation="vertical">
+                        {steps.map((step, index) => (
+                            <Step key={step.label}>
+                                <StepLabel sx={{fontSizeAdjust:"20px"}}>
+                                    {step.label}
+                                </StepLabel>
+                                <StepContent>
+                                    {getStepContent(step,index)}
+                                </StepContent>
+                            </Step>
+                        ))}
+                    </Stepper>
+                    {activeStep === steps.length && (
+                        <Paper square elevation={0} sx={{ p: 3 }}>
+                            <Typography>All steps completed - you&apos;re finished</Typography>
+                            <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                                Reset
+                            </Button>
+                        </Paper>
+                        
+                    )}
+                </Box>
+                                    
+                <Box sx={{ width:{xs:"90%",sm:"65%"}, display:"flex", flexDirection:"column", padding:"5%", alignItems:"center" }}>
+                    {getMobileStepperButtons(activeStep)}
+                            
+                            
                 {
-                    display:"flex",
-                    width:"100%", 
-                    justifyContent:"center",
-                    marginTop:"5vh", 
-                    flexDirection:"column",
-                    alignItems:"center"
+                    activeStep === 0 ? (
+                        <>
+                        <Box sx={
+                                {
+                                    display:"flex",
+                                    width:"100%", 
+                                    justifyContent:"center",
+                                    marginTop:"5vh", 
+                                    flexDirection:"column",
+                                    alignItems:"center"
+                                }
+                                }
+                            >
+                            <Autocomplete
+                                options={strains}
+                                id="strain"
+                                renderInput={(params) => {
+                                    return <TextField helperText={error.strain.message} error={error.strain.failed} {...params} label="Strain"/>
+                                }}
+                                getOptionLabel={(option) => {
+                                    return option.name
+                                }}
+                                onChange={handleChangeStrain}
+                                sx={theme.input.mobile.fullSize.desktop.halfSize}
+                            />
+
+                            <TextField
+                                id="amount"
+                                label="Amount %"
+                                ref={ref}
+                                onChange={handleChangeAmount}
+                                sx={theme.input.mobile.twoThirds.desktop.quarterSize}
+                                error={error.amount.failed}
+                                helperText={error.amount.message}
+                            />
+                            <Typography align='center' color={theme.textColor.lightGray}>Minimum Strains : 2</Typography>
+                        </Box>
+
+
+                            <Box sx={
+                                {
+                                    display:"flex",
+                                    width:{xs:"66%", sm:"34%"}, 
+                                    justifyContent:"space-evenly",
+                                    marginTop:"5vh", 
+                                    flexDirection:{xs:"row", sm:"column"},
+                                    alignItems:"center",
+                                }
+                            }>
+                                <Fab onClick={handleAddToMixComb} disabled={!canAdd} color="primary" aria-label="add" >
+                                    <AddIcon />
+                                </Fab>
+
+                                <Typography margin={"4%"} color={theme.textColor.darkGray}>Mix Length : {mix.products.length}</Typography>
+
+                                
+
+                            </Box>
+
+                            <Box sx={
+                                {
+                                display:"flex", 
+                                alignItems:"center", 
+                                justifyContent:"center",
+                                marginTop:"5vh",
+                                width:"100%"
+
+                                }
+                            }>
+                            <Button variant="contained" size='large' disabled={showFinal} onClick={handleSetMix}>
+                                Set Mix
+                            </Button>
+
+                        </Box>
+                                </>
+                                ) : null
                 }
-            }>
-                <Autocomplete
-                        options={strains}
-                        id="strain"
-                        renderInput={(params) => {
-                            return <TextField helperText={error.strain.message} error={error.strain.failed} {...params} label="Strain"/>
-                        }}
-                        getOptionLabel={(option) => {
-                            return option.name
-                        }}
-                        onChange={handleChangeStrain}
-                        sx={theme.input.mobile.fullSize.desktop.halfSize}
-                />
 
-                <TextField
-                    id="amount"
-                    label="Amount %"
-                    ref={ref}
-                    onChange={handleChangeAmount}
-                    sx={theme.input.mobile.twoThirds.desktop.quarterSize}
-                    error={error.amount.failed}
-                    helperText={error.amount.message}
-                />
-                <Typography align='center' color={theme.textColor.lightGray}>Minimum Strains : 2</Typography>
-            </Box>
+                            
+                            
+                            
 
+                        
 
-            <Box sx={
-                {
-                    display:"flex",
-                    width:{xs:"66%", sm:"34%"}, 
-                    justifyContent:"space-evenly",
-                    marginTop:"5vh", 
-                    flexDirection:{xs:"row", sm:"column"},
-                    alignItems:"center",
-                }
-            }>
-                <Fab onClick={handleAddToMixComb} disabled={!canAdd} color="primary" aria-label="add" >
-                    <AddIcon />
-                </Fab>
-
-                <Typography margin={"4%"} color={theme.textColor.darkGray}>Mix Length : {mix.products.length}</Typography>
-
-                
-
-            </Box>
-
-            <Box sx={
-            {
-                display:"flex", 
-                alignItems:"center", 
-                justifyContent:"center",
-                marginTop:"5vh",
-                width:"100%"
-
-            }
-        }>
-            <Button variant="contained" size='large' disabled={showFinal} onClick={handleSetMix}>
-                Set Mix
-            </Button>
-
-        </Box>
-
-        </Box>
-
-        {
-            showFinal
-            ?
-            <div>
-                <Box sx={
                         {
-                            display:"flex",
-                            width:"100%", 
-                            justifyContent:"center",
-                            marginTop:"5vh", 
-                            flexDirection:"column",
-                            alignItems:"center"
+                            activeStep === 1
+                            ?
+                            ( <>
+                                <Box sx={
+                                        {
+                                            display:"flex",
+                                            width:"100%", 
+                                            justifyContent:"center",
+                                            marginTop:"5vh", 
+                                            flexDirection:"column",
+                                            alignItems:"center"
+                                        }
+                                    }>
+                                        <TextField label="Mix Name" id="name" onChange={handleChangeFinalData} variant="outlined" sx={theme.input.mobile.fullSize.desktop.halfSize}>
+                                            
+                                        </TextField>
+
+                                        <TextField label="Mix Price" id="price" onChange={handleChangeFinalData} variant="outlined" sx={theme.input.mobile.fullSize.desktop.halfSize}>
+                                            
+                                        </TextField>
+
+                                </Box>
+                                <Box sx={
+                                    {
+                                        display:"flex", 
+                                        alignItems:"center", 
+                                        justifyContent:"center",
+                                        width:"100%",
+                                        flexDirection:"column"
+
+                                    }
+                                }>
+
+                                        <Fab color="primary" component="label" id="label" aria-label="add" sx={{marginY:"4%"}} >
+                                                <input  type="file" accept="image/*" onChange={handleChangeLabel} hidden />
+                                                <CameraIcon />
+                                        </Fab>
+                                    
+                                    <Button variant="contained" onClick={handleSendMixData} size='large' >
+                                        Save product mix
+                                    </Button>
+
+                                </Box>
+                            </>):
+                            null
                         }
-                    }>
-                        <TextField label="Mix Name" id="name" onChange={handleChangeFinalData} variant="outlined" sx={theme.input.mobile.fullSize.desktop.halfSize}>
-                            
-                        </TextField>
+                        </Box>
+                        
 
-                        <TextField label="Mix Price" id="price" onChange={handleChangeFinalData} variant="outlined" sx={theme.input.mobile.fullSize.desktop.halfSize}>
-                            
-                        </TextField>
-
-                </Box>
-                <Box sx={
-                    {
-                        display:"flex", 
-                        alignItems:"center", 
-                        justifyContent:"center",
-                        width:"100%",
-                        flexDirection:"column"
-
-                    }
-                }>
-
-                        <Fab color="primary" component="label" id="label" aria-label="add" sx={{marginY:"4%"}} >
-                                <input  type="file" accept="image/*" onChange={handleChangeLabel} hidden />
-                                <CameraIcon />
-                        </Fab>
                     
-                    <Button variant="contained" onClick={handleSendMixData} size='large' >
-                        Save product mix
-                    </Button>
-
-                </Box>
-            </div>
-            :
-            null
-        }
-        
-
-    
-        
-
-        
+                        
+                        </Box></Box>
+                        
     </div>
     
 
