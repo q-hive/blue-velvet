5e4d6121368ba187100efdf518632b9c36f9d728
LuisH86 <108955151+LuisH86@users.noreply.github.com>
2022-07-29 10:45:44 -0600

    Appbar and Sidebar test (Don't Pull yett)
    commit to save and change issue

--- src/modules/Admin/Dashboard/Dashboard.jsx
+++ src/modules/Admin/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //*MUI COMPONENTS
-import { Box, Button, useTheme } from '@mui/material'
+import { Box, Button, Stack, useTheme } from '@mui/material'
 
 //*ROUTER
 import { navigate } from '../../../utils/router'
@@ -11,7 +11,12 @@ import useAuth from '../../../contextHooks/useAuthContext'
 
 //*Routing
 import { useNavigate } from "react-router-dom";
+//Theme
 import { BV_THEME } from '../../../theme/BV-theme';
+//Appbar
+import BV_AppBar from './AppBar_Test';
+//Drawer
+import BV_Drawer from './Drawer_Test';
 
 export const Dashboard = () => {
     const {user} = useAuth()
@@ -25,6 +30,14 @@ export const Dashboard = () => {
     }
     
   return (
+    <>
+    <Box>
+        <BV_AppBar />
+        <Stack direction={"row"} justifyContent="center">
+    <BV_Drawer />
+    <Box sx={{display:"flex", flexDirection:"column", width:"100vw", height:"100vh", alignItems:"center", justifyContent:"center"}}>
+    
+
     <Box sx={{display:"flex", flexDirection:"column", width:"100vw", height:"100vh", alignItems:"center", justifyContent:"center"}}>
         <Button sx={theme.button.dashboard} id="employees" onClick={handleRedirect} variant='contained'>
             Employees
@@ -39,5 +52,9 @@ export const Dashboard = () => {
             Customer
         </Button>
     </Box>
+    </Box>
+    </Stack>
+    </Box>
+    </>
   )
 }
