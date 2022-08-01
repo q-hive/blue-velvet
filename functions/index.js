01b2170814885b8cb9802ba3c851eeb25ca59ee0
Luis Eduardo Esquivias <EsquizoDEV>
2022-07-31 20:50:54 -0500

    cors before deploy

--- functions/index.js
+++ functions/index.js
@@ -34,7 +34,7 @@ useMorgan(app);
 const originsList = ["http://localhost:3000", "https://bluevelvetdeploy.herokuapp.com"]
  
 app.use(cors({
-    origin: originsList[0],
+    origin: originsList[1],
     credentials: true
 }));
 
