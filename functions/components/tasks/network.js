75cbf5b09ac312f306b5618c4081c0e47e806fca
Luis Eduardo Esquivias <EsquizoDEV>
2022-08-11 17:54:23 -0500

    details almost handled

--- functions/components/tasks/network.js
+++ functions/components/tasks/network.js
@@ -1,12 +1,33 @@
-import express from 'express'
+import express, { response } from 'express'
 import { success, error } from '../../network/response.js'
 import { modelsValidationError } from '../../utils/errorHandler.js'
 import { isValidTaskObject } from './controller.js'
-import {createTask} from './store.js'
+import {createTask, insertManyTasks} from './store.js'
 
 const router = express.Router()
 
+router.post('/details', (req, res) => {
+    if(Array.isArray(req.body)){
+        //*Si es un arreglo es para popular base de datos
+        //*controlador insert many
+    
+        insertManyTasks(req.body.tasksDetails)
+        .then(message => {
+            response(req, res, 201, message)
+        })
+        .catch(err => {
+            //*CASTEAR ERRORES ESTOY HASTA LA MADRE DE NO GESTIONAR COMPLETOS LOS ERRORES
+        })
+    }
+})
+
 router.post('/', (req, res) => {
+    if(Array.isArray(req.body)){
+       //*Si es un arreglo son multiples tasks y son para asignarse
+       //*enviar arreglo a controlador de crear multiples tasks
+       
+    }
+    
     if(isValidTaskObject(req.body)){
         createTask(req.body)
         .then(() => {
