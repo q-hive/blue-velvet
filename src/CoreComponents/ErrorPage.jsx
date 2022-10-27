15a220d816b10710aac768ba786dd2db7ec7dee4
LuisH86 <108955151+LuisH86@users.noreply.github.com>
2022-10-26 19:05:19 -0600

    Update ErrorPage

--- src/CoreComponents/ErrorPage.jsx
+++ src/CoreComponents/ErrorPage.jsx
@@ -13,13 +13,15 @@ export const ErrorPage = ({error, resetErrorBoundary}) => {
         <Container sx={{marginTop:"15vh"}}>
             
             <Typography variant="h1" color={"secondary.dark"} >
-                Oops! Something went wrong
+                Oops!<br/> Something went wrong 
             </Typography>
+            <br/> 
             <Typography variant="h4" color={"red"}>
                 Error: {error.message}
             </Typography>
+            <br/> 
             <Button variant="contained" onClick={resetErrorBoundary}>
-                Recargar
+                Reload
             </Button>
 
         </Container>
