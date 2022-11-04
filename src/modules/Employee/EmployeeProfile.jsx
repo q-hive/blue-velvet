60ca06d6727a5ca5afd42f5e37b1cba40efabce8
LuisH86 <108955151+LuisH86@users.noreply.github.com>
2022-11-04 11:17:27 -0700

    EmployeeProfile update

--- src/modules/Employee/EmployeeProfile.jsx
+++ src/modules/Employee/EmployeeProfile.jsx
@@ -1,41 +1,98 @@
-import { Avatar, Box, Button, Container, Fade, Paper, Stack, Typography, useTheme } from '@mui/material'
+import { Avatar, Box, Button, CircularProgress, Container, Fade, Paper, Stack, Typography, useTheme, Grid, Grow } from '@mui/material'
 import React from 'react'
+import useAuth from '../../contextHooks/useAuthContext';
 import { BV_THEME } from '../../theme/BV-theme';
 
 export const Profile = () => {
      
-    const level = -5
-    const employeePic = "https://i.imgflip.com/58g25p.jpg"
+    const level = 2
+    const employeePic = "https://cdn-icons-png.flaticon.com/512/544/544514.png"
+    const chart = "https://landing.moqups.com/img/content/charts-graphs/column-charts/grouped-column-chart-for-channel-acquisition/grouped-column-chart-for-channel-acquisition-800.png"
+    const {user, credential} = useAuth()
+    const levelPercentage = (level/7) * 100
 
 
     const theme = useTheme(BV_THEME);
     return (
-        <>
        <Box component="div" minHeight="100vh" display="flex"  >
             <Fade in={true} timeout={1000} unmountOnExit>
-                    <Container maxWidth="md" >
-                        <Box display="flex" flexDirection="column" py={8} width="100%" height="100%" alignItems="center" >
+                <Container maxWidth="lg" >
+                    <Grid container spacing={3} marginTop={3}>
+                    
+                        {/* Profile Picture */}
+                        <Grow in={true} timeout={2000} unmountOnExit>
+                        <Grid item xs={4} md={3} lg={3}>
                             <Avatar
                                 alt="employee pic"
-                                src={employeePic}
-                                sx={{ width: {xs:"25vh", md:220}, height: {xs:"25vh",md:220}, alignSelf: 'center', mb:2}}
-                                
+                                src={user.image}
+                                sx={{boxShadow:BV_THEME.shadows[9], 
+                                width: {xs:"95%",sm:"85%", md:"80%",lg:"75%"}, 
+                                height: {xs:"auto"}, 
+                                alignSelf: 'center', mb:2}}
+                                        
                             />
-                            <Stack direction="row" spacing={{xs:8 ,md:30}} textAlign="center">
-                                <Typography variant="h6">Level : {level}</Typography>
-                            </Stack>
+                        </Grid>
+                        </Grow>
 
-                            <Paper sx={{width: {xs:"80%", md:450}, height: 450, m:3}}>
-                                Insert Chart Here.
+                        {/* User Info */}
+                        <Grow in={true} timeout={2000} unmountOnExit>
+                        <Grid item xs={8} md={9} lg={9}>
+                            
+                            
+                            {/* User Name obtained from useAuth */}
+                            <Typography variant={"h3"} color="primary.dark" sx={{fontSize:{xs:"2.5em",sm:"2.7em",md:"3em",lg:"3.5em"}}}>{user.name+" "+user.lname}</Typography>
+
+                            <Stack direction="row" spacing={{xs:8 ,md:2}} marginTop={1} textAlign="center" alignItems="center">
+                                <Typography variant="h6" component="div" color="secondary.light">
+                                        Level : 
+                                </Typography>
+                                <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+                                    <CircularProgress variant="determinate" color="primary" size="3em" value={levelPercentage} />
+                                    <Box
+                                            sx={{
+                                            top: 0,
+                                            left: 0,
+                                            bottom: 0,
+                                            right: 0,
+                                            position: 'absolute',
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            }}
+                                    >
+                                            <Typography variant="h6" component="div" color="secondary.light">
+                                            {level}
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                </Stack>    
+                        </Grid>
+                        </Grow>
+
+                        {/* Chart 1 */}
+                        <Grow in={true} timeout={2000} unmountOnExit>
+                        <Grid item xs={12} md={12} lg={6}>
+                            <Paper elevation={4} sx={{width: {xs:"95%",}, height: 450, m:3}}>
+                                <Typography color="gray">Illustrative chart</Typography>
+                                <Box component="img" src={chart} sx={{minWidth:"95%", maxHeight:"90%"}}></Box>
                             </Paper>
+                        </Grid>
+                        </Grow>
 
 
-                        </Box>
-                    </Container>
+                        {/* Chart 2 */}
+                        <Grow in={true} timeout={2000} unmountOnExit>
+                        <Grid item xs={12} md={12} lg={6}>
+                            <Paper elevation={4} sx={{width: {xs:"95%",}, height: 450, m:3}}>
+                                <Typography color="gray">Illustrative chart</Typography>
+                                <Box component="img" src={chart} sx={{minWidth:"95%", maxHeight:"95%"}}></Box>
+                            </Paper>
+                        </Grid>
+                        </Grow>
+                        
+                    </Grid>
+                </Container>
             </Fade>
        </Box>
-
-            
-        </>
     )
 }
