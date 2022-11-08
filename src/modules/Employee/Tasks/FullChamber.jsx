068b019879faffbbf5dabadfc5f04f2e488a8a2f
LuisH86 <108955151+LuisH86@users.noreply.github.com>
2022-11-08 10:41:06 -0700

    Minor changes

--- src/modules/Employee/Tasks/FullChamber.jsx
+++ src/modules/Employee/Tasks/FullChamber.jsx
@@ -105,7 +105,9 @@ export const FullChamber = () => {
     }
 
     const carouselChange = (index,element) => {
-        setWorkContext({...WorkContext, currentRender:index}) 
+        setWorkContext({...WorkContext, currentRender:index})
+        
+        if(index < 0){}
 
         if(index < canSeeNextTask.counter){    
             
@@ -181,8 +183,9 @@ export const FullChamber = () => {
                         let elapsed = finished - started
                         let thisBreak = {task:cycleKeys[WorkContext.current],started:started,finished:finished,elapsed:elapsed}
                         WorkContext.cicle[cycleKeys[WorkContext.current]].breaks.push(thisBreak)
+                        setWorkContext({...WorkContext})
                         TrackWorkModel.breaks.push(thisBreak)
-                        setTrackWorkModel({...TrackWorkModel, breaks:TrackWorkModel.breaks})
+                        setTrackWorkModel({...TrackWorkModel})
                         console.log("current task breaks", WorkContext.cicle[cycleKeys[WorkContext.current]].breaks)
                         console.log("trackwork model breaks ", TrackWorkModel.breaks)
                         setDialog({...dialog,open:false})}
@@ -236,6 +239,7 @@ export const FullChamber = () => {
                                 setFinished:setCanSeeNexttask,
                                 setSnack:setSnack,
                                 snack:snack,
+                                stepInList:index,
                                 updatePerformance: updateEmployeePerformance,
                                 products: getProductsByType(status)
                             })}
