162af258e6016fc3c3a0a970b5d890e7485f3c40
LuisH86 <108955151+LuisH86@users.noreply.github.com>
2023-05-24 14:44:42 -0700

    AuthContext Token Refreshment
    According to documentation, the implemented refreshAuthToken function will conditionally refresh the id token as needed, and return it appropiately when an api call is made using context variables. Also, Prettier Formatted

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,82 +1,91 @@
-import React, {createContext, useEffect, useState} from 'react'
-import '../firebaseInit.js'
-import {signOut} from 'firebase/auth'
-import auth from '../firebaseInit.js'
-import { useNavigate } from 'react-router-dom'
+import React, { createContext, useEffect, useState } from "react";
+import "../firebaseInit.js";
+import { signOut } from "firebase/auth";
+import auth from "../firebaseInit.js";
+import { useNavigate } from "react-router-dom";
+import api from "../axios.js";
+import { useErrorHandler } from "react-error-boundary";
 
-import api from '../axios.js'
-import { useErrorHandler } from 'react-error-boundary'
+const AuthContextProv = createContext();
 
-const AuthContextProv = createContext()
-
-const AuthContext = ({children}) => {
-  const [user, setUser] = useState(undefined)
-  const [loading, setLoading] = useState(true)
-  const [credential, setCredential] = useState(undefined)
-  const navigate = useNavigate()
+const AuthContext = ({ children }) => {
+  const [user, setUser] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const [credential, setCredential] = useState(undefined);
+  const navigate = useNavigate();
 
   const handleError = useErrorHandler();
 
   const logout = () => {
-      signOut(auth)
+    signOut(auth)
       .then(() => {
-        setUser(null)
-        // window.localStorage.clear()
-        navigate('/')
+        setUser(null);
+        navigate("/");
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  useEffect(() => {
-    return auth.onAuthStateChanged((u) => {
-      setLoading(() => true)
-      if(u){
-        auth.currentUser.getIdToken()
-        .then((token) => {
-          setCredential(() => {
-            return {
-              _tokenResponse:{idToken:token}
-            }
-          })
+  const refreshAuthToken = async () => {
+    try {
+      const token = await auth.currentUser.getIdToken();
+      setCredential(() => ({
+        _tokenResponse: { idToken: token },
+      }));
 
-          api.api.post(`/auth/refresh`, {}, {
-            headers:{
-              authorization: token
-            }
-          })
-          .then((response) => {
-            if(!response.data.data.isAdmin) {
-              response.data.data.user.role = "employee"
-            } else {
-              response.data.data.user.role = "admin"
-            }
-            setUser((usr) => {
-              return {...response.data.data.user}
-            })
-            setLoading(() => false)
-          }, (error)=>handleError(error))
-        })
-        .catch((err) => {
-          console.log("Error while getting token after reloading.")
-          console.log(err)
-        })
-        return 
+      const response = await api.api.post(
+        `/auth/refresh`,
+        {},
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+
+      const { data } = response.data;
+      const updatedUser = { ...data.user };
+      if (!data.isAdmin) {
+        updatedUser.role = "employee";
+      } else {
+        updatedUser.role = "admin";
       }
 
-      setLoading(() => false)
+      setUser(() => ({ ...updatedUser }));
+      setLoading(() => false);
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  useEffect(() => {
+    return auth.onAuthStateChanged(async (u) => {
+      setLoading(() => true);
+      if (u) {
+        try {
+          await refreshAuthToken();
+        } catch (err) {
+          console.log("Error while getting token after reloading.");
+          console.log(err);
+        }
+        return;
+      }
+
+      setLoading(() => false);
       setTimeout(() => {
-        setLoading(() => false)
-      }, 10000)
-    })
-  }, [])
+        setLoading(() => false);
+      }, 10000);
+    });
+  }, []);
 
   return (
-    <AuthContextProv.Provider value={{user, setUser, loading, logout, credential, setCredential}}>
-        {children}
+    <AuthContextProv.Provider
+      value={{ user, setUser, loading, logout, credential, setCredential }}
+    >
+      {children}
     </AuthContextProv.Provider>
-  )
-}
+  );
+};
 
-export {AuthContext, AuthContextProv}
+export { AuthContext, AuthContextProv };
