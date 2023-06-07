91d44129241b0776db255c742c439b5d38291049
OwenLobato <owenlobatov@gmail.com>
2023-06-06 18:48:15 -0600

    Fix headers of request helper

--- src/utils/helpers/requestsHelper.js
+++ src/utils/helpers/requestsHelper.js
@@ -1,22 +1,11 @@
 import axios from "axios";
 
-const request = async function (method, endpoint, {headers, data={}, api=true}={}) {
+const request = async function (method, endpoint, headers, {data={}, api=true}={}) {
 
   const hosts = ['http://localhost:9999', "https://bluevelvetdeploy.herokuapp.com"];
   const apiVersion = '/api/v1';
-  const access_token = true; // TODO: Get access token
 
   let url = `${hosts[process.env.REACT_APP_HOST_ORIGIN ?? 1]}`;
-
-  const newHeaders = {
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: headers?.headers.authorization || '',
-      user: headers?.headers.user || '',
-    }
-  }
-
-  if (access_token) newHeaders.Authorization = `Bearer ${access_token}`;
 
   if (api) url += apiVersion;
 
@@ -24,7 +13,7 @@ const request = async function (method, endpoint, {headers, data={}, api=true}={
     method: method,
     url: `${url}/${endpoint}`,
     data,
-    headers: newHeaders,
+    headers,
     withCredentials: true
   });
 };
